ec0621dcb564fa738f19557098483528194447f5
Harshit <165888192+Luci77777@users.noreply.github.com>
2024-05-02 10:32:39 +0530

    Update settings.py

--- youtubevideodownloader/settings.py
+++ youtubevideodownloader/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j$$gj^+z=pjjl1=apk+h(8icqib6d0d)xv=_c1pq9ee8fs-s(i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
