6d69d25d93a6a074b28fcc8228c49be8fe897d4a
Harshit <165888192+Luci77777@users.noreply.github.com>
2026-02-04 17:27:30 +0530

    Update settings.py

--- youtubevideodownloader/settings.py
+++ youtubevideodownloader/settings.py
@@ -124,8 +124,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
